c65fdefd384b51770e1f9e480e2759a2b82ed8ee
Kuldeep Shrivastava <153275375+kuldeepshrivastavv@users.noreply.github.com>
2026-08-15 15:41:54 +0530

    Time: 1 ms (99.96%) | Memory: 94.7 MB (15.78%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -6,7 +6,7 @@ class Solution {
             return 0 ;
         }
         
-            for( int i = 0 ; i < arr.length ; i++){
+            for( int i = 0 ; i < arr.length  ; i++){
             if(arr[i] < min ){
                min = arr[i] ;
             }
